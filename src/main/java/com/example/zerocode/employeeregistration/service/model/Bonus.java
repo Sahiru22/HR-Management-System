2d7f437def6f3b49adbc1119fa4ae98ae524795e
Sahiru22 <sahirubalasuriya222@gmail.com>
2025-08-04 22:15:47 +0530

    Add SonarQube GitHub Actions workflow

--- src/main/java/com/example/zerocode/employeeregistration/service/model/Bonus.java
+++ src/main/java/com/example/zerocode/employeeregistration/service/model/Bonus.java
@@ -1,6 +1,15 @@
 package com.example.zerocode.employeeregistration.service.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -8,14 +17,23 @@ import lombok.Data;
 @Table(name = "bonuses")
 public class Bonus {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String bonusType;
-    private Double bonusAmount;
-    private String bonusDate;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @ManyToOne
-    private Employee employee;
+  @NotBlank
+  @Size(max = 255)
+  private String bonusType;
+
+  @NotNull
+  @Digits(fraction = 2, integer = 8)
+  private Double bonusAmount;
+
+  @NotNull
+  private String bonusDate;
+
+  @ManyToOne
+  @NotNull
+  private Employee employee;
 
 }
